a9ebbae491013eb1c9e45aab34a4718c0429c788
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2026-02-10 17:01:36 -0500

    fix(app): throw error on being unable to get accesstoken instead of undefined

--- pkgs/app/src/providers/apiProvider.tsx
+++ pkgs/app/src/providers/apiProvider.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react'
 import { useMemo } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { Configuration, DefaultApi, ListingsApi } from 'sdk'
+import { Configuration, DefaultApi, ListingsApi, UsersApi } from 'sdk'
 import { ApiContext } from './apiContext'
 import CFG from '../config'
 
@@ -13,18 +13,16 @@ export function ApiProvider({ children }: PropsWithChildren) {
       basePath: CFG.API_URL,
       accessToken: async () => {
         if (isAuthenticated) {
-          try {
-            return await getAccessTokenSilently({
-              authorizationParams: {
-                audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-              },
-            })
-          } catch (error) {
-            console.error('Failed to get access token:', error)
-            return undefined
-          }
+          return await getAccessTokenSilently({
+            authorizationParams: {
+              audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+            },
+          })
+        } else {
+          // TODO: specific error type for unauthorized bubble up to error boundary
+          // which would trigger redirect to login page. Should catch getAccessTokenSilently too
+          throw new Error("something.")
         }
-        return undefined
       },
     })
   }, [isAuthenticated, getAccessTokenSilently])
