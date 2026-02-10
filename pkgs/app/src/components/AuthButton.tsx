80c2d695e34b59c8f0542c975f22325c936a15a8
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2026-02-10 17:24:21 -0500

    feat(app): add auth button

---
+++ pkgs/app/src/components/AuthButton.tsx
@@ -0,0 +1,23 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+export function AuthButton() {
+  const { isLoading, isAuthenticated, error, loginWithRedirect, logout } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        Logout
+      </button>
+    );
+  } else {
+    return <button onClick={() => loginWithRedirect()}>Login</button>;
+  }
+}
